6fd58e861a96e30cf6bd619d03d1d4c761c5d373
saranya <ssaaranya8@gmail.com>
2026-03-11 18:39:48 +0530

    Initial portfolio website upload

---
+++ script.js
@@ -0,0 +1,65 @@
+// TYPING EFFECT
+
+const text = [
+"AI Student",
+"Data Science Enthusiast",
+"Machine Learning Developer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+count++;
+index = 0;
+}
+
+setTimeout(type,120);
+
+})();
+
+
+// NAVBAR ACTIVE SECTION
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+
+let current = "";
+
+sections.forEach(section => {
+
+const sectionTop = section.offsetTop - 150;
+const sectionHeight = section.clientHeight;
+
+if(pageYOffset >= sectionTop){
+current = section.getAttribute("id");
+}
+
+});
+
+navLinks.forEach(link => {
+
+link.classList.remove("active");
+
+if(link.getAttribute("href") === "#" + current){
+link.classList.add("active");
+}
+
+});
+
+});
